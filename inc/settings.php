463bafb423ae2fd7ed23d575916330bef2c746f8
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-05 14:15:44 +0700

    Settings: Add tabs

--- inc/settings.php
+++ inc/settings.php
@@ -35,6 +35,22 @@ function register_menu(): void {
 }
 
 /**
+ * Get settings sections
+ *
+ * @since 0.0.1
+ *
+ * @return array
+ */
+function get_sections(): array {
+	return [
+		[
+			'id' => 'general',
+			'title' => __( 'General' ),
+		],
+	];
+}
+
+/**
  * Render settings page
  *
  * @since 0.0.1
@@ -44,6 +60,19 @@ function register_menu(): void {
 function render_page(): void {
 	?>
 	<h1><?php esc_html_e( 'Catatan Settings', 'catatan' ); ?></h1>
+
+	<ul class="nav-tab-wrapper wp-clearfix" aria-label="<?php esc_attr_e( 'Secondary menu' ); ?>">
+		<?php foreach ( get_sections() as $index => $section ) : ?>
+			<?php
+			printf(
+				'<button class="nav-tab %s">%s</button>',
+				esc_attr( $index === 0 ? 'nav-tab-active' : '' ),
+				esc_html( $section['title'] )
+			);
+			?>
+		<?php endforeach; ?>
+	</ul>
+
 	<form action="options.php" method="post">
 		<?php submit_button(); ?>
 	</form>
