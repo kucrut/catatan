ae851f745541dba459c58cf9719e32c5806e464b
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-06 14:02:53 +0700

    Editor: Split config generator

--- inc/editor.php
+++ inc/editor.php
@@ -53,53 +53,23 @@ function register_menu( string $post_type, WP_Post_Type $post_type_object ): voi
 			1
 		);
 
-		add_action( "load-{$hook}", fn() => load( $post_type ) );
+		add_action( "load-{$hook}", fn () => load( $post_type ) );
 	};
 
 	add_action( 'admin_menu', $callback );
 }
 
 /**
- * Load editor page
+ * Get editor config
  *
- * @since 0.0.1
- *
- * @param string $post_type Current post type.
+ * @param string $post_type Post type name.
  *
  * @return void
  */
-function load( string $post_type ): void {
-	enqueue_assets();
+function get_config( string $post_type ) {
+	$post_type_object = get_post_type_object( $post_type );
 
-	add_action( 'admin_print_scripts', __NAMESPACE__ . '\\print_assets' );
-}
-
-/**
- * Enqueue assets
- *
- * @since 0.0.1
- *
- * @return void
- */
-function enqueue_assets(): void {
-	wp_enqueue_style( 'wp-components' );
-	wp_enqueue_style( 'wp-edit-post' );
-}
-
-/**
- * Print assets
- *
- * TODO: Use vite-for-wp for this
- *
- * @since 0.0.1
- *
- * @return void
- */
-function print_assets(): void {
-	$screen = get_current_screen();
-	$post_type_object = get_post_type_object( $screen->post_type ? $screen->post_type : 'post' );
-
-	$data = [
+	$config = [
 		'editor_id' => CATATAN\EDITOR_ID,
 		// phpcs:disable WordPress.Security.NonceVerification.Recommended
 		'post_id' => isset( $_REQUEST['id'] ) ? (int) $_REQUEST['id'] : 0,
@@ -122,9 +92,61 @@ function print_assets(): void {
 			'visibility' => __( 'Visibility' ),
 		],
 	];
+
+	/**
+	 * Filter editor config
+	 *
+	 * @since 0.0.1
+	 *
+	 * @param array $config Editor config.
+	 */
+	$config = apply_filters( 'catatan__editor_config', $config );
+
+	return $config;
+}
+
+/**
+ * Load editor page
+ *
+ * @since 0.0.1
+ *
+ * @param string $post_type Current post type.
+ *
+ * @return void
+ */
+function load( string $post_type ): void {
+	enqueue_assets();
+
+	add_action( 'admin_print_scripts', fn () => print_assets( $post_type ) );
+}
+
+/**
+ * Enqueue assets
+ *
+ * @since 0.0.1
+ *
+ * @return void
+ */
+function enqueue_assets(): void {
+	wp_enqueue_style( 'wp-components' );
+	wp_enqueue_style( 'wp-edit-post' );
+}
+
+/**
+ * Print assets
+ *
+ * TODO: Use vite-for-wp for this
+ *
+ * @since 0.0.1
+ *
+ * @param string $post_type Current post type.
+ *
+ * @return void
+ */
+function print_assets( string $post_type ): void {
 	?>
 <script>
-	var catatanEditor = <?php echo wp_json_encode( $data ); ?>;
+	var catatanEditor = <?php echo wp_json_encode( get_config( $post_type ) ); ?>;
 </script>
 <?php // phpcs:disable WordPress.WP.EnqueuedResources.NonEnqueuedScript ?>
 <script type="module" src="http://localhost:5173/@vite/client"></script>
