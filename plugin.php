d59ef345895dd5fd2a00554faa05f15e0e144d52
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-06 13:55:31 +0700

    Remove obsolete code

--- plugin.php
+++ plugin.php
@@ -11,13 +11,11 @@ declare( strict_types = 1 );
 
 namespace Catatan;
 
-require_once __DIR__ . '/inc/admin-menu.php';
 require_once __DIR__ . '/inc/editor.php';
 require_once __DIR__ . '/inc/namespace.php';
 require_once __DIR__ . '/inc/overrides.php';
 require_once __DIR__ . '/inc/settings.php';
 
-Admin_Menu\bootstrap();
 Editor\bootstrap();
 Overrides\bootstrap();
 Settings\bootstrap();
