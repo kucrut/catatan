5385079289c3afb2f595ff43247e927d1e307d45
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-07 02:29:13 +0700

    Document store: Split editor state and changes

--- packages/editor/src/lib/stores/document.ts
+++ packages/editor/src/lib/stores/document.ts
@@ -15,6 +15,8 @@ export interface DocumentStoreValue {
 	data: Changes;
 	is_dirty: boolean;
 	is_saved: boolean;
+	is_saving: boolean;
+	was_saving: boolean;
 }
 
 export type DocumentStore = ReturnType< typeof create_document_store >;
@@ -68,21 +70,21 @@ function create_original_post_store( post_id: number, rest_path: string ) {
 }
 
 export default function create_document_store( { post_id, rest_path }: DocumentStoreParams ) {
-	const original = create_original_post_store( post_id, rest_path );
+	const changes_store = persist( writable< Changes >( {} ), localStorage(), `catatan-document-${ post_id }` );
+	const original_store = create_original_post_store( post_id, rest_path );
 
-	const { update, ...store } = persist(
-		writable< DocumentStoreValue >( {
-			data: {},
-			is_dirty: false,
-			is_saved: false,
-		} ),
-		localStorage(),
-		`catatan-document-${ post_id }`,
-	);
+	const { update, ...editor } = writable< DocumentStoreValue >( {
+		data: {},
+		is_dirty: false,
+		is_saved: false,
+		is_saving: false,
+		was_saving: false,
+	} );
 
-	original.subscribe( ( { content, status, title } ) => {
-		update( value => ( {
-			...value,
+	// Update editor's data when original store value is updated.
+	original_store.subscribe( ( { content, status, title } ) => {
+		update( $editor => ( {
+			...$editor,
 			data: {
 				status,
 				content: content?.raw || '',
@@ -92,33 +94,43 @@ export default function create_document_store( { post_id, rest_path }: DocumentS
 		} ) );
 	} );
 
+	// Update editor's data when changes store value is updated.
+	changes_store.subscribe( $changes =>
+		update( ( { data, ...$document } ) => ( {
+			...$document,
+			data: { ...data, ...$changes },
+		} ) ),
+	);
+
 	let current_value: DocumentStoreValue;
 
-	store.subscribe( value => ( current_value = value ) );
+	editor.subscribe( value => ( current_value = value ) );
 
 	return {
-		...store,
-		fetch: original.fetch,
+		...editor,
+		fetch: original_store.fetch,
 
 		async save() {
 			try {
-				await original.save( current_value.data );
-				update( value => ( { ...value, is_saved: true } ) );
+				update( $editor => ( { ...$editor, is_saving: true, was_saving: false } ) );
+				await original_store.save( current_value.data );
+				update( $editor => ( { ...$editor, is_saved: true, was_saving: true } ) );
 			} catch ( error ) {
 				// TODO: Display error in notice section.
 				// eslint-disable-next-line no-console
 				console.error( error );
+			} finally {
+				update( $editor => ( { ...$editor, is_saving: false } ) );
 			}
 		},
 
-		update( changes: Changes ) {
-			update( ( { data, ...rest }: DocumentStoreValue ) => ( {
-				...rest,
-				data: {
-					...data,
-					...changes,
-				},
+		update( new_changes: Changes ) {
+			changes_store.update( $changes => ( { ...$changes, ...new_changes } ) );
+			update( $editor => ( {
+				...$editor,
 				is_dirty: true,
+				is_saved: false,
+				was_saving: false,
 			} ) );
 		},
 	};
