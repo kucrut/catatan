93ec216b1a60e95523441fae7cc72a31a2e5e4e2
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-06 21:58:05 +0700

    Create document context (document store)

--- packages/editor/src/App.svelte
+++ packages/editor/src/App.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import { setContext } from 'svelte';
 	import Container from '$lib/components/container.svelte';
+	import create_document_store from '$lib/stores/document';
 	import type { Config } from '$types';
 
 	export let config: Config;
 
-	const { l10n } = config;
+	const { l10n, post_id } = config;
 
+	setContext( 'document', create_document_store( post_id ) );
 	setContext( 'l10n', l10n );
 </script>
 
