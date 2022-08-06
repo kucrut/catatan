5ad1dc66e0a58a685e05158d709bc6007b515be0
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-06 22:23:45 +0700

    Connect content input with document store

--- packages/editor/src/lib/components/editor.svelte
+++ packages/editor/src/lib/components/editor.svelte
@@ -1,5 +1,20 @@
+<script lang="ts">
+	import { getContext } from 'svelte';
+	// import type { Config } from '$types';
+	import type { DocumentStore } from '$lib/stores/document';
+
+	const doc = getContext< DocumentStore >( 'document' );
+	// const l10n = getContext< Config[ 'l10n' ] >( 'l10n' );
+
+	function handle_change( event: Event & { currentTarget: HTMLTextAreaElement } ) {
+		doc.update( { content: event.currentTarget.value } );
+	}
+</script>
+
 <div class="is-root-container block-editor-block-list__layout">
-	<textarea class="widefat" />
+	<textarea class="widefat" on:change={handle_change}
+		>{$doc.changes.content || $doc.original.content?.raw || ''}</textarea
+	>
 </div>
 
 <style>
