618171b5fd34e4d1199917d5c6f8ef5e4dfb75ce
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-07 02:30:15 +0700

    Save button: Handle all kinds of states

--- packages/editor/src/lib/components/save-button.svelte
+++ packages/editor/src/lib/components/save-button.svelte
@@ -7,15 +7,43 @@
 	const doc = getContext< DocumentStore >( 'document' );
 	const l10n = getContext< Config[ 'l10n' ] >( 'l10n' );
 
-	let text: string;
+	$: cls = '';
+	$: icon = '';
+	$: is_disabled = true;
+	$: text = l10n.save_draft;
 
 	$: {
-		if ( $doc.data.status === 'draft' ) {
-			text = $doc.is_saved ? l10n.saved : l10n.save_draft;
-		} else {
+		if ( $doc.is_saved ) {
+			cls = 'is-saved';
+		}
+
+		if ( $doc.was_saving ) {
+			icon = 'check';
+			text = l10n.saved;
+		}
+
+		if ( $doc.is_dirty ) {
+			cls = '';
+			icon = '';
+			is_disabled = false;
+			text = l10n.save_draft;
+		}
+
+		if ( $doc.is_saving ) {
+			cls = 'is-saving components-animate__loading';
+			icon = 'cloud';
+			is_disabled = true;
+			text = l10n.saving;
+		}
+
+		if ( $doc.data.status && $doc.data.status !== 'draft' ) {
 			text = l10n.switch_to_draft;
 		}
+
+		cls += ' editor-post-saved-state';
 	}
 </script>
 
-<Button is_tertiary aria-disabled={! $doc.is_dirty} disabled={! $doc.is_dirty}>{text}</Button>
+<Button {icon} is_tertiary aria-disabled={is_disabled} class={cls} disabled={is_disabled} on:click={() => doc.save()}
+	>{text}</Button
+>
