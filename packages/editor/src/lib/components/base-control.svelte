6278cd63971ddfb96294976f72187aa40d45c192
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-07 21:36:53 +0700

    Add component: BaseControl

---
+++ packages/editor/src/lib/components/base-control.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	export let id = '';
+	export let label = '';
+</script>
+
+<div class="components-base-control">
+	<div class="components-base-control__field">
+		{#if id && label}
+			<label class="components-base-control__label" for={id}>{label}</label>
+		{/if}
+		<slot />
+	</div>
+</div>
+
+<style>
+	.components-base-control__field,
+	label {
+		margin-block-end: 0.5rem;
+	}
+
+	label {
+		display: inline-block;
+	}
+</style>
