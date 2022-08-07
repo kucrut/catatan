c25219af86681fcc9e3b5f515feaae0900c25146
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-07 21:39:58 +0700

    Editor store: Add slug to data

--- packages/editor/src/lib/stores/editor.ts
+++ packages/editor/src/lib/stores/editor.ts
@@ -82,12 +82,15 @@ export default function create_document_store( { edit_link_template, post_id, re
 	} );
 
 	// Update editor's data when original store value is updated.
-	original_store.subscribe( ( { content, id, link, status, title } ) => {
+	original_store.subscribe( $original => {
+		const { content, id, link, slug, status, title } = $original;
+
 		update( $editor => ( {
 			...$editor,
 			data: {
 				id,
 				link,
+				slug,
 				status,
 				content: content?.raw || '',
 				title: title?.raw || '',
