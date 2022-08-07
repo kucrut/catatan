5e68a5aa20a63c979280cb60ed1dc0134c2d21a8
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-07 21:40:28 +0700

    PermalinkPanel: Use components

--- packages/editor/src/lib/components/permalink-panel.svelte
+++ packages/editor/src/lib/components/permalink-panel.svelte
@@ -1,14 +1,35 @@
 <script lang="ts">
 	import { getContext } from 'svelte';
+	import BaseControl from './base-control.svelte';
+	import ExternalLink from './external-link.svelte';
 	import Panel from './panel.svelte';
 	import PanelRow from './panel-row.svelte';
 	import type { Config } from '$types';
+	import type { EditorStore } from '$lib/stores/editor';
 
+	const id = 'catatan-post-slug-input';
+	const editor = getContext< EditorStore >( 'editor' );
 	const l10n = getContext< Config[ 'l10n' ] >( 'l10n' );
 </script>
 
 <Panel title={l10n.permalink} let:is_expanded>
 	<PanelRow {is_expanded}>
-		<p>{l10n.url_slug}</p>
+		<div class="editor-post-link">
+			<BaseControl {id} label={l10n.url_slug}>
+				<input
+					{id}
+					autocomplete="off"
+					class="components-text-control__input"
+					spellcheck="false"
+					type="text"
+					value={$editor.data.slug}
+				/>
+			</BaseControl>
+			<p>
+				The last part of the URL. <ExternalLink href="https://wordpress.org/support/article/settings-sidebar/#permalink"
+					>Read about permalinks</ExternalLink
+				>
+			</p>
+		</div>
 	</PanelRow>
 </Panel>
