2481cb5a5ccde08e65b81159efe2558ac5a0a5b8
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-08 19:53:54 +0700

    ContentInput: Use autosize

--- packages/editor/src/lib/components/content-input.svelte
+++ packages/editor/src/lib/components/content-input.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
 	import { getContext } from 'svelte';
 	// import type { Config } from '$types';
+	import textarea_autosize from '@github/textarea-autosize';
 	import type { EditorStore } from '$lib/stores/editor';
 
 	const editor = getContext< EditorStore >( 'editor' );
 	// const l10n = getContext< Config[ 'l10n' ] >( 'l10n' );
+
+	function autosize( node: HTMLTextAreaElement ) {
+		textarea_autosize( node );
+	}
 
 	function handle_input( event: Event & { currentTarget: HTMLTextAreaElement } ) {
 		editor.update( { content: event.currentTarget.value } );
@@ -12,7 +17,7 @@
 </script>
 
 <div class="is-root-container block-editor-block-list__layout">
-	<textarea class="widefat" on:input={handle_input}>{$editor.data.content}</textarea>
+	<textarea class="widefat" on:input={handle_input} use:autosize>{$editor.data.content}</textarea>
 </div>
 
 <style>
