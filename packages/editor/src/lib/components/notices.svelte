71a672cc608e3eddd55686eb0ab3a85474ea0920
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-09 22:32:32 +0700

    Notices: Render items

--- packages/editor/src/lib/components/notices.svelte
+++ packages/editor/src/lib/components/notices.svelte
@@ -1,3 +1,21 @@
-<div class="interface-interface-skeleton__notices">
-	<div class="components-snackbar-list components-editor-notices__snackbar" tabindex="-1" />
-</div>
+<script lang="ts">
+	import { getContext } from 'svelte';
+	import Notice from './notice.svelte';
+	import type { NoticesStore } from '$lib/stores/notices';
+
+	const notices = getContext< NoticesStore >( 'notices' );
+
+	$: dismissible_items = $notices.filter( ( { dismissible } ) => dismissible );
+</script>
+
+{#if $notices.length}
+	<div class="interface-interface-skeleton__notices">
+		{#if dismissible_items.length}
+			<div class="components-notice-list components-editor-notices__dismissible">
+				{#each dismissible_items as item}
+					<Notice {item} />
+				{/each}
+			</div>
+		{/if}
+	</div>
+{/if}
