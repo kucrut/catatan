d9e0806967e016f87eee21b792126c8a979f9790
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-09 23:43:43 +0700

    Add component: Snackbar

---
+++ packages/editor/src/lib/components/snackbar.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import { __ } from '@wordpress/i18n';
+	import { getContext, onMount } from 'svelte';
+	import Button from './button.svelte';
+	import type { Notice, NoticesStore } from '$lib/stores/notices';
+
+	export let item: Notice;
+
+	const notices = getContext< NoticesStore >( 'notices' );
+
+	const { content, id, link } = item;
+	let timeout_id: number;
+
+	function cancel_scheduled_removal() {
+		if ( timeout_id ) {
+			clearTimeout( timeout_id );
+		}
+	}
+
+	function schedule_removal() {
+		cancel_scheduled_removal();
+		timeout_id = setTimeout( () => {
+			notices.remove( id );
+		}, 1500 );
+	}
+
+	onMount( schedule_removal );
+</script>
+
+<div
+	aria-label={__( 'Dismiss this notice' )}
+	class="components-snackbar"
+	role="button"
+	tabindex="0"
+	on:mouseenter={() => cancel_scheduled_removal()}
+	on:mouseleave={() => schedule_removal()}
+>
+	<div class="components-snackbar__content">
+		{content}
+		{#if link}
+			<Button is_tertiary class="components-snackbar__action" href={link.url} target="_blank">{link.text}</Button>
+		{/if}
+	</div>
+</div>
