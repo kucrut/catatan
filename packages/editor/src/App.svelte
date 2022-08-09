09be21bd5e8b6899a47ac79dcac425bf8f8d675a
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-09 22:22:40 +0700

    App: Init notices_store

--- packages/editor/src/App.svelte
+++ packages/editor/src/App.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import Container from '$lib/components/container.svelte';
+	import create_notices_store, { type NoticesStore } from '$lib/stores/notices';
 	import create_editor_store, { type EditorStore } from '$lib/stores/editor';
 	import create_post_store, { type PostStore } from '$lib/stores/post';
 	import create_post_type_store, { type PostTypeStore } from '$lib/stores/post-type';
@@ -13,18 +14,20 @@
 	let editor_store: EditorStore;
 	let is_ready = false;
 	let loading_error: Error;
+	let notices_store: NoticesStore;
 	let post_store: PostStore;
 	let post_type_store: PostTypeStore;
 
 	onMount( async () => {
 		try {
+			notices_store = create_notices_store();
 			post_type_store = create_post_type_store( post_type );
 			await post_type_store.fetch();
 
 			post_store = create_post_store( post_id, $post_type_store );
 			await post_store.fetch();
 
-			editor_store = create_editor_store( { post_id, post_store, ...editor_config } );
+			editor_store = create_editor_store( { notices_store, post_id, post_store, ...editor_config } );
 			is_ready = true;
 		} catch ( error ) {
 			loading_error = error;
@@ -33,7 +36,7 @@
 </script>
 
 {#if is_ready}
-	<Container {editor_store} {l10n} {post_store} {post_type_store} />
+	<Container {editor_store} {l10n} {notices_store} {post_store} {post_type_store} />
 {:else if loading_error}
 	<p>{loading_error.message}</p>
 {:else}
