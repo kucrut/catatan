41962d5ca7123ffee5ca97a55887c2f189cc6e85
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-14 13:45:00 +0700

    Header: Decouple toggle sidebar button

--- packages/editor/src/lib/components/header.svelte
+++ packages/editor/src/lib/components/header.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
 	import { __ } from '@wordpress/i18n';
-	import Button from './button.svelte';
 	import PreviewButton from './preview-button.svelte';
 	import PublishButton from './publish-button.svelte';
 	import SaveButton from './save-button.svelte';
-	import ui from '$lib/stores/ui';
+	import ToggleSidebarButton from './toggle-sidebar-button.svelte';
 </script>
 
 <div aria-label={__( 'Editor top bar' )} class="interface-interface-skeleton__header" role="region" tabindex="-1">
@@ -24,14 +23,7 @@
 			<PreviewButton />
 			<PublishButton />
 			<div class="interface-pinned-items">
-				<Button
-					aria-expanded={$ui.is_sidebar_open}
-					aria-label={__( 'Settings' )}
-					aria-pressed={$ui.is_sidebar_open}
-					class={$ui.is_sidebar_open ? 'is-pressed' : ''}
-					icon="gear"
-					on:click={() => ui.toggle_sidebar()}
-				/>
+				<ToggleSidebarButton />
 			</div>
 		</div>
 	</div>
---
+++ packages/editor/src/lib/components/toggle-sidebar-button.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { __ } from '@wordpress/i18n';
+	import Button from './button.svelte';
+	import ui from '$lib/stores/ui';
+</script>
+
+<Button
+	aria-expanded={$ui.is_sidebar_open}
+	aria-label={__( 'Settings' )}
+	aria-pressed={$ui.is_sidebar_open}
+	class={$ui.is_sidebar_open ? 'is-pressed' : ''}
+	icon="gear"
+	on:click={() => ui.toggle_sidebar()}
+/>
