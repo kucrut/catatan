986d9c799b12605a4e568481358328d8a58db7ca
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-19 16:10:17 +0700

    StatusPanel: Update check

--- packages/editor/src/lib/components/status-panel.svelte
+++ packages/editor/src/lib/components/status-panel.svelte
@@ -4,20 +4,20 @@
 	import TrashButton from './trash-button.svelte';
 	import Panel from './panel.svelte';
 	import PanelRow from './panel-row.svelte';
-	import type { EditorStore } from '$lib/stores/editor';
+	import type { PostStore } from '$lib/stores/post';
 
-	const editor = getContext< EditorStore >( 'editor' );
+	const post = getContext< PostStore >( 'post' );
 </script>
 
 <!-- TODO: Remove this check when we have other rows ready. -->
-{#if editor.user_can( 'delete' ) && $editor.data.id}
+{#if $post.__can__.delete}
 	<Panel title={__( 'Status & visibility' )}>
 		<!-- <PanelRow>
-		<span>{__( 'Visibility' )}</span>
-	</PanelRow>
-	<PanelRow>
-		<span>{__( 'Publish' )}</span>
-	</PanelRow> -->
+			<span>{__( 'Visibility' )}</span>
+		</PanelRow>
+		<PanelRow>
+			<span>{__( 'Publish' )}</span>
+		</PanelRow> -->
 		<PanelRow>
 			<TrashButton />
 		</PanelRow>
