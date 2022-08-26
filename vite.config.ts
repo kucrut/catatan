aa55fdc3a5da7a389fc6edcd02e2035ef4f2fda0
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-26 11:52:20 +0700

    Add wp globals to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { resolve } from 'node:path';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { wp_globals } from '@kucrut/vite-for-wp/utils';
 import create_config from '@kucrut/vite-for-wp';
+import external_globals from 'rollup-plugin-external-globals';
 
 const config = create_config( 'app/src/main.ts', 'app/dist', {
-	plugins: [ svelte() ],
+	plugins: [ external_globals( wp_globals() ), svelte() ],
 	resolve: {
 		alias: {
 			$lib: resolve( 'app/src/lib' ),
