8122d3bd464b542767f99284d0fb0519a50ea368
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-26 22:51:29 +0700

    Taxonomies store add params

--- app/src/lib/stores/taxonomies.ts
+++ app/src/lib/stores/taxonomies.ts
@@ -1,34 +1,35 @@
-import { derived, writable } from 'svelte/store';
+import { derived, writable, type Readable } from 'svelte/store';
 import api_fetch from '@wordpress/api-fetch';
 import type { WP_REST_API_Taxonomies } from 'wp-types';
+import with_params from './with-params';
 
 interface Params {
 	post_type: string;
 }
 
+export interface Store< T > extends Readable< T > {
+	fetch(): Promise< void >;
+}
+
 function create_store() {
 	const tax_store = writable< WP_REST_API_Taxonomies >();
 	const store = derived( tax_store, $tax_store => $tax_store );
-	const params = writable< Params >( { post_type: '' } );
-	let $params: Params;
-
-	params.subscribe( $value => ( $params = $value ) );
+	const params = with_params< Params >();
 
 	return {
 		...store,
-
-		set_params( arg: Params ) {
-			params.update( () => arg );
-		},
+		...params,
 
 		async fetch() {
-			if ( ! $params.post_type ) {
+			const { post_type } = this.get_params();
+
+			if ( ! post_type ) {
 				throw new Error( '[Taxonomies store] Error: Post type is not set.' );
 			}
 
 			const data = await api_fetch< WP_REST_API_Taxonomies >( {
 				parse: true,
-				path: `/wp/v2/taxonomies/?type=${ $params.post_type }?context=edit`,
+				path: `/wp/v2/taxonomies/?type=${ post_type }?context=edit`,
 			} );
 
 			tax_store.update( () => data );
