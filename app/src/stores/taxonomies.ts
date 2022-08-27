6ea8361394f356231e6337999e9175635c6686ea
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-28 01:44:19 +0700

    Taxonomies store: Add permissions

--- app/src/stores/taxonomies.ts
+++ app/src/stores/taxonomies.ts
@@ -1,40 +1,62 @@
 import type { PostStore } from './post';
-import type { WP_REST_API_Taxonomies } from 'wp-types';
+import type { WP_REST_API_Taxonomy } from 'wp-types';
 import api_fetch from '@wordpress/api-fetch';
-import { derived, writable, type Readable } from 'svelte/store';
+import { derived, writable, type Readable, type Writable } from 'svelte/store';
 
 export interface Permission {
 	assign: boolean;
 	create: boolean;
 }
 
-export interface TaxonomiesStore extends Readable< WP_REST_API_Taxonomies > {
+export interface Taxonomy extends WP_REST_API_Taxonomy {
+	__can__: Permission;
+}
+
+export type Taxonomies = Taxonomy[];
+
+export interface TaxonomiesStore extends Readable< Taxonomies > {
 	fetch(): Promise< void >;
 }
 
+const actions = [ 'assign', 'create' ];
+
 export default function create_store( post: PostStore ): TaxonomiesStore {
-	const tax_store = writable< WP_REST_API_Taxonomies >();
-	const store = derived( tax_store, $tax_store => $tax_store );
+	const tax_store = writable< Taxonomies >();
+	const store = derived< [ Writable< Taxonomies >, PostStore ], Taxonomies >(
+		[ tax_store, post ],
+		( [ $tax_store, $post ], set ) => {
+			const { _links } = $post;
+
+			const taxonomies = $tax_store.map( tax => {
+				const { rest_base } = tax;
+				const permissions = actions.map( action => {
+					const key = `wp:action-${ action }-${ rest_base }`;
+					return [ action, key in _links ];
+				} );
+
+				return {
+					...tax,
+					__can__: Object.fromEntries( permissions ),
+				};
+			} );
+
+			set( taxonomies );
+		},
+	);
 	let post_type: string;
 
-	post.subscribe( ( { type } ) => {
-		post_type = type;
-	} );
+	post.subscribe( ( { type } ) => ( post_type = type ) );
 
 	return {
 		...store,
 
 		async fetch(): Promise< void > {
-			if ( ! post_type ) {
-				return;
-			}
-
-			const data = await api_fetch< WP_REST_API_Taxonomies >( {
+			const data = await api_fetch< Taxonomies >( {
 				parse: true,
 				path: `/wp/v2/taxonomies?context=edit&type=${ post_type }`,
 			} );
 
-			tax_store.update( () => data );
+			tax_store.update( () => Object.values( data ) );
 		},
 	};
 }
