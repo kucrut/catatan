8011a890f8659a618ff2108ffef4739d76e2a7e2
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-28 02:25:08 +0700

    Add component: HierarchicalTermsPanel

---
+++ app/src/components/hierarchical-terms-panel.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { Taxonomy } from '$stores/taxonomies';
+	// import CheckboxControl from './checkbox-control.svelte';
+	import Panel from './panel.svelte';
+
+	export let taxonomy: Taxonomy;
+</script>
+
+<Panel title={taxonomy.name}>
+	<div class="editor-post-taxonomies__hierarchical-terms-list">
+		<div class="editor-post-taxonomies__hierarchical-terms-choice">
+			<!-- <CheckboxControl id="bleh" label="boohooo" /> -->
+		</div>
+	</div>
+</Panel>
