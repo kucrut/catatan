3fd3aec39af4713848b42e503d965224235532db
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-28 02:44:49 +0700

    Add component: FlatTermsPanel

---
+++ app/src/components/flat-terms-panel.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { Taxonomy } from '$stores/taxonomies';
+	import FormTokenField from './form-token-field.svelte';
+	import Panel from './panel.svelte';
+	import { __ } from '@wordpress/i18n';
+
+	export let taxonomy: Taxonomy;
+
+	const { labels, rest_base } = taxonomy;
+	const { add_new_item } = labels;
+</script>
+
+<Panel title={taxonomy.name}>
+	<FormTokenField help={__( 'Separate with commas or the Enter key.' )} id={rest_base} label={add_new_item} />
+</Panel>
