c5d3579fdabb90f55b9d13dfadc82874d05fd1b1
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-28 01:14:13 +0700

    Taxonomies store: Accept post store

--- app/src/stores/taxonomies.ts
+++ app/src/stores/taxonomies.ts
@@ -1,19 +1,34 @@
+import type { PostStore } from './post';
 import type { WP_REST_API_Taxonomies } from 'wp-types';
 import api_fetch from '@wordpress/api-fetch';
 import { derived, writable, type Readable } from 'svelte/store';
+
+export interface Permission {
+	assign: boolean;
+	create: boolean;
+}
 
 export interface TaxonomiesStore extends Readable< WP_REST_API_Taxonomies > {
 	fetch(): Promise< void >;
 }
 
-export default function create_store( post_type: string ): TaxonomiesStore {
+export default function create_store( post: PostStore ): TaxonomiesStore {
 	const tax_store = writable< WP_REST_API_Taxonomies >();
 	const store = derived( tax_store, $tax_store => $tax_store );
+	let post_type: string;
+
+	post.subscribe( ( { type } ) => {
+		post_type = type;
+	} );
 
 	return {
 		...store,
 
 		async fetch(): Promise< void > {
+			if ( ! post_type ) {
+				return;
+			}
+
 			const data = await api_fetch< WP_REST_API_Taxonomies >( {
 				parse: true,
 				path: `/wp/v2/taxonomies?context=edit&type=${ post_type }`,
