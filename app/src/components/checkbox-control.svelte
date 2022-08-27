1961987bd46f7927e519524f9146cc990b4cb02f
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-28 02:13:35 +0700

    Add component: CheckboxControl

---
+++ app/src/components/checkbox-control.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import BaseControl from './base-control.svelte';
+
+	export let id: string;
+	export let label: string;
+</script>
+
+<BaseControl {id} {label} type="checkbox">
+	<span class="components-checkbox-control__input-container" slot="before-label">
+		<input {id} class="components-checkbox-control__input" type="checkbox" value="1" />
+	</span>
+</BaseControl>
