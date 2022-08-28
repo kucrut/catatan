0d4a44b6b2f071618727d49affdd47c1f9a75bd3
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-29 01:22:05 +0700

    HierarchicalTermsChoice: Accept pure, sorted array of terms

--- app/src/components/hierarchical-terms-choice.svelte
+++ app/src/components/hierarchical-terms-choice.svelte
@@ -1,19 +1,17 @@
 <script lang="ts">
 	import type { Taxonomy } from '$stores/taxonomies';
-	import type { TermsStore } from '$stores/terms';
+	import type { TermWithChildren } from '$stores/terms';
 	import CheckboxControl from './checkbox-control.svelte';
 	import { get_store } from '$stores';
 
 	export let class_prefix: string;
-	export let parent = 0;
 	export let taxonomy: Taxonomy;
-	export let terms: TermsStore;
+	export let terms: TermWithChildren[];
 
 	const editor = get_store( 'editor' );
 
 	$: ( { rest_base: tax_name } = taxonomy );
 	$: selected = $editor.data[ tax_name ] as number[];
-	$: choices = $terms ? $terms.filter( ( { parent: parent_id } ) => parent === parent_id ) : [];
 
 	function handle_check( event: Event & { target: HTMLInputElement } ): void {
 		const id = Number( event.target.value );
@@ -26,8 +24,7 @@
 	}
 </script>
 
-{#each choices as { id, name } (id)}
-	{@const  children = $terms.filter( ( { parent: parent_id } ) => id === parent_id ) }
+{#each terms as { children, id, name } (id)}
 	<div class="{class_prefix}-choice">
 		<CheckboxControl
 			checked={selected.includes( id )}
@@ -36,9 +33,9 @@
 			value={id}
 			on:change={handle_check}
 		/>
-		{#if children.length}
+		{#if children && children.length}
 			<div class="{class_prefix}-subchoices">
-				<svelte:self {class_prefix} {taxonomy} {terms} parent={id} />
+				<svelte:self {class_prefix} {taxonomy} terms={children} />
 			</div>
 		{/if}
 	</div>
