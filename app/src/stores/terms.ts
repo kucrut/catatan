90df4bfda4cf2ba8fc5d5c06d35352f08657b547
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-28 22:23:18 +0700

    Terms store: Add method: create

--- app/src/stores/terms.ts
+++ app/src/stores/terms.ts
@@ -2,7 +2,14 @@ import type { WP_REST_API_Term } from 'wp-types';
 import api_fetch from '@wordpress/api-fetch';
 import { derived, writable, type Readable } from 'svelte/store';
 
+export interface NewTerm {
+	name: string;
+	parent?: number;
+}
+
 export interface TermsStore extends Readable< WP_REST_API_Term[] > {
+	// eslint-disable-next-line no-unused-vars
+	create( data: NewTerm ): Promise< WP_REST_API_Term >;
 	fetch(): Promise< void >;
 }
 
@@ -12,6 +19,24 @@ export default function create_store( url: string ): TermsStore {
 
 	return {
 		...store,
+
+		async create( data: NewTerm ): Promise< WP_REST_API_Term > {
+			try {
+				const new_term = await api_fetch< WP_REST_API_Term >( {
+					url,
+					body: JSON.stringify( data ),
+					headers: { 'Content-Type': 'application/json; charset=UTF-8' },
+					method: 'POST',
+					parse: true,
+				} );
+
+				terms_store.update( $terms => [ new_term, ...$terms ] );
+
+				return new_term;
+			} catch ( error ) {
+				throw error;
+			}
+		},
 
 		async fetch(): Promise< void > {
 			const data = await api_fetch< WP_REST_API_Term[] >( {
