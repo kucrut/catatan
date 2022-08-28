120974473da25758a3824a4a28b8a68de403c742
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-29 01:20:18 +0700

    Terms store: Store flat and sorted terms

--- app/src/stores/terms.ts
+++ app/src/stores/terms.ts
@@ -7,15 +7,60 @@ export interface NewTerm {
 	parent?: number;
 }
 
-export interface TermsStore extends Readable< WP_REST_API_Term[] > {
+export interface TermWithChildren extends WP_REST_API_Term {
+	children?: WP_REST_API_Term[];
+}
+
+interface StoreValue {
+	flat: WP_REST_API_Term[];
+	sorted?: TermWithChildren[];
+}
+
+export interface TermsStore extends Readable< StoreValue > {
 	// eslint-disable-next-line no-unused-vars
 	create( data: NewTerm ): Promise< WP_REST_API_Term >;
 	fetch(): Promise< void >;
 }
 
-export default function create_store( url: string ): TermsStore {
+// Credit: https://stackoverflow.com/a/31384614
+function flat_to_hierarchy( flat: TermWithChildren[] ): TermWithChildren[] {
+	const keyed_by_id = Object.fromEntries( flat.map( term => [ term.id, term ] ) );
+	const result: TermWithChildren[] = [];
+
+	Object.values( keyed_by_id ).forEach( item => {
+		const parent = Number( item.parent );
+
+		if ( parent === 0 ) {
+			result.push( item );
+		} else if ( parent in keyed_by_id ) {
+			const p = keyed_by_id[ parent ];
+
+			if ( ! ( 'children' in p ) ) {
+				p.children = [];
+			}
+
+			p.children.push( item );
+		}
+	} );
+
+	return result;
+}
+
+export default function create_store( api_url: string, is_hierarchical = false ): TermsStore {
 	const terms_store = writable< WP_REST_API_Term[] >();
-	const store = derived( terms_store, $terms_store => $terms_store );
+	const store = derived< typeof terms_store, StoreValue >( terms_store, ( $terms, set ) => {
+		const flat = $terms || [];
+
+		if ( ! ( is_hierarchical && $terms ) ) {
+			set( { flat } );
+			return;
+		}
+
+		set( {
+			flat,
+			sorted: flat_to_hierarchy( flat ),
+		} );
+	} );
 
 	return {
 		...store,
@@ -23,7 +68,7 @@ export default function create_store( url: string ): TermsStore {
 		async create( data: NewTerm ): Promise< WP_REST_API_Term > {
 			try {
 				const new_term = await api_fetch< WP_REST_API_Term >( {
-					url,
+					url: api_url,
 					body: JSON.stringify( data ),
 					headers: { 'Content-Type': 'application/json; charset=UTF-8' },
 					method: 'POST',
@@ -41,7 +86,7 @@ export default function create_store( url: string ): TermsStore {
 		async fetch(): Promise< void > {
 			const data = await api_fetch< WP_REST_API_Term[] >( {
 				parse: true,
-				url: `${ url }?context=edit&per_page=100`,
+				url: `${ api_url }?context=edit&per_page=100`,
 			} );
 			terms_store.update( () => data );
 			// TODO: Fetch more.
