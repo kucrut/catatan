e22fe45460a9a0c6dbf61a93f4f3b2b71d8cf6da
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-28 15:52:07 +0700

    CheckboxControl: Add icon if checked

--- app/src/components/checkbox-control.svelte
+++ app/src/components/checkbox-control.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
 	import BaseControl from './base-control.svelte';
+	import Icon from './icons/icon.svelte';
 
+	export let checked = false;
 	export let id: string;
 	export let label: string;
 </script>
 
 <BaseControl {id} {label} type="checkbox">
 	<span class="components-checkbox-control__input-container" slot="before-label">
-		<input {id} class="components-checkbox-control__input" type="checkbox" {...$$restProps} on:change />
+		<input {checked} {id} class="components-checkbox-control__input" type="checkbox" {...$$restProps} on:change />
+		{#if checked}
+			<Icon class="components-checkbox-control__checked" icon="check" />
+		{/if}
 	</span>
 </BaseControl>
