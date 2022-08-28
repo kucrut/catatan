c7c476dead85e6d6b95098a50c1a3fde2425b7b9
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-28 22:28:34 +0700

    HierarchicalTermsPanel: Add support for creating new term

--- app/src/components/hierarchical-terms-panel.svelte
+++ app/src/components/hierarchical-terms-panel.svelte
@@ -1,21 +1,26 @@
 <script lang="ts">
 	import type { SelectControlOption } from '$types';
 	import type { Taxonomy } from '$stores/taxonomies';
-	import type { TermsStore } from '$stores/terms';
+	import type { NewTerm, TermsStore } from '$stores/terms';
 	import HierarchicalTermsChoice from './hierarchical-terms-choice.svelte';
 	import Panel from './panel.svelte';
 	import SelectControl from './select-control.svelte';
 	import TextControl from './text-control.svelte';
+	import { get_store } from '$stores';
+	import { sprintf, __ } from '@wordpress/i18n';
 
 	export let taxonomy: Taxonomy;
 	export let terms: TermsStore;
 
+	const editor = get_store( 'editor' );
+	const notices = get_store( 'notices' );
 	const class_prefix = 'editor-post-taxonomies__hierarchical-terms';
 	let is_creating_term = false;
+	let is_create_button_disabled = true;
 	let term_options: SelectControlOption[];
 
-	$: ( { labels, name, slug, __can__ } = taxonomy );
-	$: ( { add_new_item, parent_item, new_item_name } = labels );
+	$: ( { labels, name, rest_base, slug, __can__ } = taxonomy );
+	$: ( { add_new_item, parent_item, singular_name, new_item_name } = labels );
 	$: {
 		if ( $terms ) {
 			term_options = [
@@ -23,13 +28,49 @@
 					label: `— ${ parent_item } —`,
 					value: '',
 				},
-				...$terms.map( ( { id, name } ) => ( { label: name, value: id } ) ),
+				...$terms.map( ( { id, name: label } ) => ( { label, value: id } ) ),
 			];
 		}
 	}
 
 	function handle_click_toggle(): void {
 		is_creating_term = ! is_creating_term;
+	}
+
+	function handle_change_new_term_name( event: Event & { target: HTMLInputElement } ): void {
+		is_create_button_disabled = event.target.value === '';
+	}
+
+	async function handle_create_new_term( event: SubmitEvent ): Promise< void > {
+		const form = event.target as HTMLFormElement;
+		const data = new FormData( form );
+		const params: NewTerm = { name: '' };
+		const term_name = data.get( 'name' ) as string;
+		const term_parent = data.get( 'parent' ) as string;
+
+		if ( ! term_name ) {
+			return;
+		}
+
+		params.name = term_name;
+
+		if ( term_parent ) {
+			params.parent = Number( term_parent );
+		}
+
+		try {
+			const { id } = await terms.create( params );
+
+			editor.add_term( rest_base, id );
+			form.reset();
+		} catch ( error ) {
+			notices.add( {
+				content: sprintf( __( `Create %s error: %s.` ), singular_name.toLocaleLowerCase(), error.message ),
+				dismissible: true,
+				id: `create-${ name }-error`,
+				type: 'error',
+			} );
+		}
 	}
 </script>
 
@@ -45,18 +86,28 @@
 			on:click={handle_click_toggle}>{add_new_item}</button
 		>
 		{#if is_creating_term}
-			<div>
-				<TextControl class="{class_prefix}-input" id="{class_prefix}-{slug}-input" label={new_item_name} />
+			<form on:submit|preventDefault={handle_create_new_term}>
+				<TextControl
+					autocomplete="off"
+					class="{class_prefix}-input"
+					id="{class_prefix}-{slug}-input"
+					label={new_item_name}
+					name="name"
+					on:input={handle_change_new_term_name}
+				/>
 				{#if term_options.length}
 					<SelectControl
 						id="{class_prefix}-{slug}-parent"
 						label={parent_item}
+						name="parent"
 						options={term_options}
 						value={term_options[ 0 ].value}
 					/>
 				{/if}
-				<button class="components-button {class_prefix}-submit is-secondary">{add_new_item}</button>
-			</div>
+				<button class="components-button {class_prefix}-submit is-secondary" disabled={is_create_button_disabled}
+					>{add_new_item}</button
+				>
+			</form>
 		{/if}
 	{/if}
 </Panel>
