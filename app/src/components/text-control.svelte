906f350d31c9c423ece1e5f6b031ff99aff8bf03
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-28 20:15:24 +0700

    Add component: TextControl

---
+++ app/src/components/text-control.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import BaseControl from './base-control.svelte';
+
+	let cls = '';
+	export { cls as class };
+	export let id: string;
+	export let label: string;
+</script>
+
+<BaseControl {id} {label} class={cls}>
+	<input {id} class="components-text-control__input" type="text" {...$$restProps} on:change />
+</BaseControl>
