640b2e93f118c82611a5af8417ea3a851b677bd0
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-30 18:11:43 +0700

    Refactor editor backend loader

--- inc/editor.php
+++ inc/editor.php
@@ -5,6 +5,7 @@ namespace Catatan\Editor;
 
 use Catatan;
 use Kucrut\Vite;
+use WP_Post;
 use WP_Post_Type;
 
 /**
@@ -83,19 +84,21 @@ function register_page( WP_Post_Type $post_type, bool $for_edit = true ): void {
  *
  * @since 0.1.0
  *
- * @return int
+ * @return int|null Post ID or null if not found or invalid.
  */
-function get_post_id(): int {
-	// phpcs:ignore WordPress.Security.NonceVerification.Recommended
-	if ( isset( $_GET['id'] ) ) {
-		// phpcs:ignore WordPress.Security.NonceVerification.Recommended
-		$post_id = (int) $_GET['id'];
+function get_post_id(): ?int {
+	$post_id = null;
 
-		if ( $post_id < 1 ) {
-			$post_id = 0;
-		}
-	} else {
-		$post_id = 0;
+	// phpcs:ignore WordPress.Security.NonceVerification.Recommended
+	if ( ! isset( $_GET['id'] ) ) {
+		return $post_id;
+	}
+
+	// phpcs:ignore WordPress.Security.NonceVerification.Recommended
+	$found_id = (int) $_GET['id'];
+
+	if ( $found_id > 0 ) {
+		$post_id = $found_id;
 	}
 
 	return $post_id;
@@ -104,28 +107,23 @@ function get_post_id(): int {
 /**
  * Get editor config
  *
+ * @param WP_Post      $post      Current post object being edited.
  * @param WP_Post_Type $post_type Post type object.
  *
  * @return array
  */
-function get_config( WP_Post_Type $post_type ): array {
-	$post_id = get_post_id();
+function get_config( WP_Post $post, WP_Post_Type $post_type ): array {
 	$post_list_url = admin_url( 'edit.php' );
 
 	if ( $post_type->name !== 'post' ) {
 		$post_list_url = add_query_arg( [ 'post_type' => $post_type->name ], $post_list_url );
 	}
 
-	if ( $post_id === 0 ) {
-		$post = get_default_post_to_edit( $post_type->name, true );
-		$post_id = $post->ID;
-	}
-
 	$config = [
 		'edit_link_template' => preg_replace( '/(\d+)$/', '<id>', Catatan\get_editor_url( $post_type->name, 1 ) ),
 		'editor_id' => CATATAN\EDITOR_ID,
 		'nonce' => wp_create_nonce( 'wp_rest' ),
-		'post_id' => $post_id,
+		'post_id' => $post->ID,
 		'post_list_url' => $post_list_url,
 		'post_rest_path' => sprintf( '%s/%s', $post_type->rest_namespace, $post_type->rest_base ),
 		'post_type' => $post_type->name,
@@ -139,10 +137,10 @@ function get_config( WP_Post_Type $post_type ): array {
 	 * @since 0.1.0
 	 *
 	 * @param array        $config    Editor config.
+	 * @param WP_Post      $post      Current post object being edited.
 	 * @param WP_Post_Type $post_type Post type object.
-	 * @param int          $post_id   Post ID being edited (0 on the create post screen).
 	 */
-	$config = apply_filters( 'catatan__editor_config', $config, $post_type, $post_id );
+	$config = apply_filters( 'catatan__editor_config', $config, $post, $post_type );
 
 	return $config;
 }
@@ -160,18 +158,60 @@ function get_config( WP_Post_Type $post_type ): array {
  * @return void
  */
 function load( WP_Post_Type $post_type, bool $is_edit = true ): void {
+	if ( ! current_user_can( $post_type->cap->edit_posts ) || ! current_user_can( $post_type->cap->create_posts ) ) {
+		wp_die(
+			'<h1>' . esc_html__( 'You need a higher level of permission.' ) . '</h1>' .
+			'<p>' . esc_html__( 'Sorry, you are not allowed to create posts as this user.' ) . '</p>',
+			403
+		);
+	}
+
+	$post_id = get_post_id();
+
+	if ( $is_edit ) {
+		// Ensure we're on the correct page.
+		if ( empty( $post_id ) ) {
+			wp_safe_redirect( Catatan\get_editor_url( $post_type->name ), 302, 'Catatan' );
+			exit;
+		}
+
+		$post = get_post( $post_id );
+
+		if ( empty( $post ) ) {
+			wp_die( __( 'You attempted to edit an item that does not exist. Perhaps it was deleted?' ) );
+		}
+	} else {
+		$post = get_default_post_to_edit( $post_type->name, true );
+	}
+
+	if ( $post->post_type !== $post_type->name ) {
+		wp_die(
+			esc_html__( 'A post type mismatch has been detected.' ),
+			esc_html__( 'Sorry, you are not allowed to edit this item.' ),
+			400
+		);
+	}
+
+	if ( ! current_user_can( 'edit_post', $post->ID ) ) {
+		wp_die( esc_html__( 'Sorry, you are not allowed to edit this item.' ) );
+	}
+
+	if ( 'trash' === $post->post_status ) {
+		wp_die( esc_html__( 'You cannot edit this item because it is in the Trash. Please restore it and try again.' ) );
+	}
+
 	/**
 	 * Fire before the load actions are run
 	 *
 	 * @since 0.1.0
 	 *
+	 * @param WP_Post      $post      Current post object being edited.
 	 * @param WP_Post_Type $post_type Current post type object.
-	 * @param bool         $is_edit  Are we loading the edit page?
+	 * @param bool         $is_edit   Are we loading the edit page?
 	 */
-	do_action( 'catatan__before_load_editor', $post_type, $is_edit );
+	do_action( 'catatan__before_load_editor', $post, $post_type, $is_edit );
 
-	check_permission( $post_type, $is_edit );
-	enqueue_assets( $post_type );
+	enqueue_assets( $post, $post_type );
 
 	if ( $is_edit ) {
 		// Because we've removed the page from admin menus, WP does not have the page title anymore, so we need to fix it here.
@@ -190,78 +230,16 @@ function load( WP_Post_Type $post_type, bool $is_edit = true ): void {
 }
 
 /**
- * Check permission
- *
- * @since 0.1.0
- *
- * The checks are mostly taken from wp-admin/post.php.
- *
- * @todo Check for post lock.
- *
- * @param WP_Post_Type $post_type Current post type object.
- * @param bool         $is_edit   Is this the edit page?
- *
- * @return void
- */
-function check_permission( WP_Post_Type $post_type, bool $is_edit = true ): void {
-	if ( ! current_user_can( $post_type->cap->edit_posts ) || ! current_user_can( $post_type->cap->create_posts ) ) {
-		wp_die(
-			'<h1>' . esc_html__( 'You need a higher level of permission.' ) . '</h1>' .
-			'<p>' . esc_html__( 'Sorry, you are not allowed to create posts as this user.' ) . '</p>',
-			403
-		);
-	}
-
-	if ( ! $is_edit ) {
-		return;
-	}
-
-	// From here on, we're dealing with edit screen.
-
-	$post_id = get_post_id();
-
-	if ( $post_id === 0 ) {
-		wp_safe_redirect( Catatan\get_editor_url( $post_type->name ), 302, 'Catatan' );
-		exit;
-	}
-
-	if ( $post_id < 1 ) {
-		wp_die( esc_html__( 'Invalid post ID.', 'catatan' ) );
-	}
-
-	$post = get_post( $post_id );
-
-	if ( ! $post ) {
-		wp_die( esc_html__( 'You attempted to edit an item that does not exist. Perhaps it was deleted?' ) );
-	}
-
-	if ( $post->post_type !== $post_type->name ) {
-		wp_die(
-			esc_html__( 'A post type mismatch has been detected.' ),
-			esc_html__( 'Sorry, you are not allowed to edit this item.' ),
-			400
-		);
-	}
-
-	if ( ! current_user_can( 'edit_post', $post_id ) ) {
-		wp_die( esc_html__( 'Sorry, you are not allowed to edit this item.' ) );
-	}
-
-	if ( 'trash' === $post->post_status ) {
-		wp_die( esc_html__( 'You cannot edit this item because it is in the Trash. Please restore it and try again.' ) );
-	}
-}
-
-/**
  * Enqueue assets
  *
  * @since 0.1.0
  *
+ * @param WP_Post      $post      Current post object being edited.
  * @param WP_Post_Type $post_type Current post type object.
  *
  * @return void
  */
-function enqueue_assets( WP_Post_Type $post_type ): void {
+function enqueue_assets( WP_Post $post, WP_Post_Type $post_type ): void {
 	wp_enqueue_global_styles_css_custom_properties();
 
 	Vite\enqueue_asset(
@@ -278,7 +256,7 @@ function enqueue_assets( WP_Post_Type $post_type ): void {
 	wp_add_inline_script(
 		Catatan\EDITOR_ID,
 		// phpcs:ignore WordPress.WP.AlternativeFunctions.json_encode_json_encode
-		sprintf( 'var catatanEditor = %s;', json_encode( get_config( $post_type ) ) ),
+		sprintf( 'var catatanEditor = %s;', json_encode( get_config( $post, $post_type ) ) ),
 		'before'
 	);
 }
