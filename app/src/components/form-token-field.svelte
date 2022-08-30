5bd055dd56a0ee3ff679c8ec3e351c7e85dc319a
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-30 20:37:49 +0700

    FormTokenField: Toggle is-active class on focus/blur

--- app/src/components/form-token-field.svelte
+++ app/src/components/form-token-field.svelte
@@ -4,13 +4,18 @@
 	export let help = '';
 
 	const class_prefix = 'components-form-token';
+	let is_focused = false;
+
+	function toggle_is_focused() {
+		is_focused = ! is_focused;
+	}
 </script>
 
 <div class="{class_prefix}-field">
 	<label for="{class_prefix}-input-{id}" class="{class_prefix}-field__label">{label}</label>
-	<div class="{class_prefix}-field__input-container">
+	<div class="{class_prefix}-field__input-container" class:is-active={is_focused}>
 		<input
-			aria-describedby={help ? '{class_prefix}-suggestions-howto-{id}' : null}
+			aria-describedby={help ? `${ class_prefix }-suggestions-howto-${ id }` : null}
 			aria-expanded="false"
 			autocomplete="off"
 			class="{class_prefix}-field__input"
@@ -19,6 +24,8 @@
 			size="5"
 			type="text"
 			value=""
+			on:blur={toggle_is_focused}
+			on:focus={toggle_is_focused}
 		/>
 	</div>
 	{#if help}
