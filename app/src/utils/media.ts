3ab2c9686739ff2724d8ab73464f93b1691fede2
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-01 02:04:34 +0700

    Custom featured image controller to work around core's bug

--- app/src/utils/media.ts
+++ app/src/utils/media.ts
@@ -1,9 +1,65 @@
+/* eslint-disable object-shorthand, prefer-rest-params, @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-function-return-type */
+
 // Borrowed from Gutenberg's source.
 
 const { wp } = window;
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function get_attachments_collection( ids: number[] ): any {
+const FeaturedImageController = wp.media.controller.FeaturedImage.extend( {
+	// The core version has a bug; it always sets the selection to the _saved_ featured image
+	// even though we're opening the frame for the second time after changing the featured image.
+	activate: function () {
+		wp.media.controller.Library.prototype.activate.apply( this, arguments );
+	},
+} );
+
+export const FeaturedImageFrame = wp.media.view.MediaFrame.Select.extend( {
+	/**
+	 * Enables the Set Featured Image Button.
+	 *
+	 * @param {Object} toolbar toolbar for featured image state
+	 * @return {void}
+	 */
+	featuredImageToolbar( toolbar ): void {
+		this.createSelectToolbar( toolbar, {
+			text: wp.media.view.l10n.setFeaturedImage,
+			state: this.options.state,
+		} );
+	},
+
+	/**
+	 * Handle the edit state requirements of selected media item.
+	 */
+	editState(): void {
+		const selection = this.state( 'featured-image' ).get( 'selection' );
+		const view = new wp.media.view.EditImage( {
+			model: selection.single(),
+			controller: this,
+		} ).render();
+
+		// Set the view to the EditImage frame using the selected image.
+		this.content.set( view );
+
+		// After bringing in the frame, load the actual editor via an ajax call.
+		view.loadEditor();
+	},
+
+	/**
+	 * Create the default states.
+	 */
+	createStates: function createStates(): void {
+		this.on( 'toolbar:create:featured-image', this.featuredImageToolbar, this );
+		this.on( 'content:render:edit-image', this.editState, this );
+
+		this.states.add( [
+			new FeaturedImageController(),
+			new wp.media.controller.EditImage( {
+				model: this.options.editImage,
+			} ),
+		] );
+	},
+} );
+
+export function get_attachments_collection( ids: number[] ) {
 	return wp.media.query( {
 		order: 'ASC',
 		orderby: 'post__in',
@@ -11,57 +67,5 @@ export function get_attachments_collection( ids: number[] ): any {
 		posts_per_page: -1,
 		query: true,
 		type: 'image',
-	} );
-}
-
-/**
- * Prepare the Featured Image toolbars and frames.
- */
-export function get_featured_image_media_frame(): typeof wp.media.view.MediaFrame.Select {
-	return wp.media.view.MediaFrame.Select.extend( {
-		/**
-		 * Enables the Set Featured Image Button.
-		 *
-		 * @param {Object} toolbar toolbar for featured image state
-		 * @return {void}
-		 */
-		featuredImageToolbar( toolbar ): void {
-			this.createSelectToolbar( toolbar, {
-				text: wp.media.view.l10n.setFeaturedImage,
-				state: this.options.state,
-			} );
-		},
-
-		/**
-		 * Handle the edit state requirements of selected media item.
-		 */
-		editState(): void {
-			const selection = this.state( 'featured-image' ).get( 'selection' );
-			const view = new wp.media.view.EditImage( {
-				model: selection.single(),
-				controller: this,
-			} ).render();
-
-			// Set the view to the EditImage frame using the selected image.
-			this.content.set( view );
-
-			// After bringing in the frame, load the actual editor via an ajax call.
-			view.loadEditor();
-		},
-
-		/**
-		 * Create the default states.
-		 */
-		createStates: function createStates(): void {
-			this.on( 'toolbar:create:featured-image', this.featuredImageToolbar, this );
-			this.on( 'content:render:edit-image', this.editState, this );
-
-			this.states.add( [
-				new wp.media.controller.FeaturedImage(),
-				new wp.media.controller.EditImage( {
-					model: this.options.editImage,
-				} ),
-			] );
-		},
 	} );
 }
