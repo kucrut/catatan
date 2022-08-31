33be227fb3bfd361b40d70daeb1cb0a947814cf0
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-31 21:02:40 +0700

    Add types: WP_Media, WP_Media_Orientation, WP_Media_Size

--- app/src/types/index.d.ts
+++ app/src/types/index.d.ts
@@ -26,6 +26,68 @@ export interface TokenItem {
 	label: string;
 }
 
+export type WP_Media_Orientation = 'landscape' | 'portrait';
+
+export interface WP_Media_Size {
+	height: number;
+	width: number;
+	url: string;
+	orientation: WP_Media_Orientation;
+}
+
+export interface WP_Media {
+	alt: string;
+	author: string;
+	authorLink: string;
+	authorName: string;
+	caption: string;
+	context: string;
+	date: string;
+	dateFormatted: string;
+	description: string;
+	editLink: string;
+	filename: string;
+	filesizeHumanReadable: string;
+	filesizeInBytes: number;
+	height: number;
+	icon: string;
+	id: number;
+	link: string;
+	menuOrder: number;
+	meta: boolean;
+	mime: string;
+	modified: string;
+	name: string;
+	orientation: WP_Media_Orientation;
+	originalImageName: string;
+	originalImageURL: string;
+	status: string;
+	subtype: string;
+	title: string;
+	type: string;
+	uploadedTo: number;
+	uploadedToLink: string;
+	uploadedToTitle: string;
+	url: string;
+	width: number;
+	compat?: {
+		item: string;
+		meta: string;
+	};
+	nonces?: {
+		update?: string;
+		delete?: string;
+		edit?: string;
+	};
+	sizes: {
+		full: WP_Media_Size;
+		large?: WP_Media_Size;
+		medium?: WP_Media_Size;
+		thumbnail?: WP_Media_Size;
+		[ k: string ]: WP_Media_Size;
+	};
+}
+
 declare global {
 	interface Window {
 		catatanEditor: Config;
