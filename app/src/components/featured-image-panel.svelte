639429c0bea453071854110b826042386f23629f
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-31 19:06:56 +0700

    FeaturedImagePanel: Render button if there's no image set

--- app/src/components/featured-image-panel.svelte
+++ app/src/components/featured-image-panel.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
 	import Panel from './panel.svelte';
 	import { __ } from '@wordpress/i18n';
+	import { get_store } from '$stores';
+
+	const editor = get_store( 'editor' );
 </script>
 
 <Panel title={__( 'Featured image' )}>
 	<div class="editor-post-featured-image">
-		<button type="button" class="components-button editor-post-featured-image__toggle"
-			>{__( 'Set featured image' )}</button
-		>
+		{#if ! $editor.data.featured_media}
+			<button type="button" class="components-button editor-post-featured-image__toggle"
+				>{__( 'Set featured image' )}</button
+			>
+		{/if}
 	</div>
 </Panel>
