8c419aaa99092688445f02739b2a9694e5d2552f
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-31 23:53:20 +0700

    Header: Only render PreviewButton if post type is viewable

--- app/src/components/header.svelte
+++ app/src/components/header.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
+	import { get_store } from '$stores';
 	import { __ } from '@wordpress/i18n';
 	import PreviewButton from './preview-button.svelte';
 	import PublishButton from './publish-button.svelte';
 	import SaveButton from './save-button.svelte';
 	import ToggleSidebarButton from './toggle-sidebar-button.svelte';
+
+	const post_type = get_store( 'post_type' );
 </script>
 
 <div aria-label={__( 'Editor top bar' )} class="interface-interface-skeleton__header" role="region">
@@ -20,7 +23,9 @@
 		</div>
 		<div class="edit-post-header__settings">
 			<SaveButton />
-			<PreviewButton />
+			{#if $post_type.viewable}
+				<PreviewButton />
+			{/if}
 			<PublishButton />
 			<div class="interface-pinned-items">
 				<ToggleSidebarButton />
