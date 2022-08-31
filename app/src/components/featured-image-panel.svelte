80def3fff627f4a98969fb9d2f3221a32619a8d4
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-31 21:16:44 +0700

    FeaturedImagePanel: Select media using MediaFrame

--- app/src/components/featured-image-panel.svelte
+++ app/src/components/featured-image-panel.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import MediaFrame from './media-frame.svelte';
 	import Panel from './panel.svelte';
 	import { __ } from '@wordpress/i18n';
 	import { get_store } from '$stores';
@@ -10,6 +11,15 @@
 	const DEFAULT_TITLE = __( 'Featured image' );
 	const class_prefix = 'editor-post-featured-image';
 
+	let is_frame_open = false;
+
+	const close_frame = () => ( is_frame_open = false );
+	const open_frame = () => ( is_frame_open = true );
+
+	function handle_select( event: CustomEvent< { selection: object[] } > ): void {
+		editor.update( { featured_media: event.detail.selection[ 0 ].id } );
+	}
+
 	$: set_label = $post_type.labels.set_featured_image || DEFAULT_SET_LABEL;
 	$: title = $post_type.labels.featured_image || DEFAULT_TITLE;
 </script>
@@ -17,7 +27,10 @@
 <Panel {title}>
 	<div class={class_prefix}>
 		{#if ! $editor.data.featured_media}
-			<button type="button" class="components-button {class_prefix}__toggle">{set_label}</button>
+			<button class="components-button {class_prefix}__toggle" type="button" on:click={open_frame}>{set_label}</button>
+		{/if}
+		{#if is_frame_open}
+			<MediaFrame on:close={close_frame} on:select={handle_select} />
 		{/if}
 	</div>
 </Panel>
