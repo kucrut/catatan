d8c8091ed934a02ecd1e40a36cef8fd7d8e6f791
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-31 19:04:28 +0700

    Add component: FeaturedImagePanel

---
+++ app/src/components/featured-image-panel.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Panel from './panel.svelte';
+	import { __ } from '@wordpress/i18n';
+</script>
+
+<Panel title={__( 'Featured image' )}>
+	<div class="editor-post-featured-image">
+		<button type="button" class="components-button editor-post-featured-image__toggle"
+			>{__( 'Set featured image' )}</button
+		>
+	</div>
+</Panel>
