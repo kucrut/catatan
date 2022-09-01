ab141d811b452a47fe55369f7ba93b0cff5455d5
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-01 19:08:20 +0700

    Revert "Vite config: Add base"
    This reverts commit 270d7590fda30ce8d23922b2954c4525c9003caa.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import create_config from '@kucrut/vite-for-wp';
 import external_globals from 'rollup-plugin-external-globals';
 
 const config = create_config( 'app/src/main.ts', 'app/dist', {
-	base: './',
 	plugins: [ external_globals( wp_globals() ), svelte() ],
 	resolve: {
 		alias: {
