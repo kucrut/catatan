ce06d6a569da69e114927b5298322d258dfa8713
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-01 16:26:49 +0700

    Add component: Lazy

---
+++ app/src/components/lazy.svelte
@@ -0,0 +1,7 @@
+<script>
+	export let component;
+</script>
+
+{#await component.then ? component : component() then Component}
+	<svelte:component this={Component.default} {...$$restProps} />
+{/await}
