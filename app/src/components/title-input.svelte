dfb318d3e4f7f68e8c8995c8988f4c54ae47d774
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-03 01:10:47 +0700

    TitleInput: Use contenteditable H1

--- app/src/components/title-input.svelte
+++ app/src/components/title-input.svelte
@@ -2,37 +2,70 @@
 	import { __ } from '@wordpress/i18n';
 	import { get_store } from '$stores';
 
-	const editor = get_store( 'editor' );
+	type TitleInputEvent = Event & { currentTarget: HTMLHeadingElement };
 
-	function handle_input( event: InputEvent & { currentTarget: HTMLInputElement } ): void {
-		editor.update( { title: event.currentTarget.value } );
+	const editor = get_store( 'editor' );
+	const placeholder = __( 'Add title' );
+
+	function handle_input( event: TitleInputEvent ): void {
+		editor.update( { title: event.currentTarget.innerText } );
+	}
+
+	function handle_keyup( event: TitleInputEvent ): void {
+		// Because we're using a block element (h1), the browser will most probably insert a <br/> tag.
+		if ( event.currentTarget.innerText === '\n' ) {
+			// Let's clean it up.
+			event.currentTarget.innerHTML = '';
+		}
+	}
+
+	function handle_paste( event: ClipboardEvent & { currentTarget: HTMLHeadingElement } ): void {
+		// We don't want _any_ markup in our post title.
+		event.currentTarget.innerText = event.clipboardData.getData( 'text' );
+		// We've prevented the paste event, so let's trigger the input event :wink:
+		event.currentTarget.dispatchEvent( new Event( 'input' ) );
 	}
 </script>
 
-<div class="edit-post-visual-editor__post-title-wrapper">
+<div class="edit-post-visual-editor__post-title-wrapper" contenteditable="false">
 	<label for="post-title" class="screen-reader-text">{__( 'Post Title' )}</label>
-	<input
-		class="wp-block wp-block-post-title block-editor-block-list__block editor-post-title editor-post-title__input"
-		id="post-title"
-		placeholder={__( 'Add title' )}
-		value={$editor.data.title}
+	<h1
+		aria-label={placeholder}
+		aria-multiline="true"
+		class="wp-block wp-block-post-title block-editor-block-list__block editor-post-title editor-post-title__input rich-text"
+		contenteditable="true"
+		data-placeholder={placeholder}
+		role="textbox"
 		on:input={handle_input}
-	/>
+		on:keyup={handle_keyup}
+		on:paste|preventDefault={handle_paste}
+	>
+		{$editor.data.title}
+	</h1>
 </div>
 
-<style>
+<style lang="postcss">
 	.edit-post-visual-editor__post-title-wrapper {
 		margin-block-start: 2rem;
 		margin-block-end: 0.5rem;
 	}
 
-	input {
-		width: 100%;
-		border: unset;
-		font-weight: 300;
-		/* stylelint-disable custom-property-pattern */
+	h1 {
+		white-space: pre-wrap;
+		min-width: 1px;
+		margin-block: unset;
+		margin-inline: auto;
 		font-family: var( --wp--preset--font-family--source-serif-pro );
-		font-size: var( --wp--preset--font-size--x-large );
+		font-size: var( --wp--custom--typography--font-size--gigantic );
+		font-weight: 300;
 		line-height: var( --wp--custom--typography--line-height--tiny );
+
+		&:empty::before {
+			content: attr( data-placeholder );
+			pointer-events: none;
+			display: block;
+			position: absolute;
+			opacity: 0.62;
+		}
 	}
 </style>
