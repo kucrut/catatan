38c092ac5b0e977ba3cba82cddc5fff33c094f52
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-04 05:51:32 +0700

    Add component: Popover

---
+++ app/src/components/popover.svelte
@@ -0,0 +1,7 @@
+<div tabindex="-1" class="components-popover components-animate__appear is-from-top is-from-center is-without-arrow">
+	<div class="components-popover__content">
+		<div style="position: relative;">
+			<slot />
+		</div>
+	</div>
+</div>
