f2e0eb49b8d07e9ef40ccc2ae11ed33824b5f41f
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-04 18:22:34 +0700

    Popover: Accept props

--- app/src/components/popover.svelte
+++ app/src/components/popover.svelte
@@ -1,7 +1,21 @@
-<div tabindex="-1" class="components-popover components-animate__appear is-from-top is-from-center is-without-arrow">
-	<div class="components-popover__content">
-		<div style="position: relative;">
-			<slot />
-		</div>
+<script lang="ts">
+	export let animate: null | 'appear' = null;
+	export let is_from_center = false;
+	export let is_from_top = false;
+	export let is_without_arrow = false;
+
+	const class_prefix = 'components-popover';
+</script>
+
+<div
+	class={class_prefix}
+	tabindex="-1"
+	class:components-animate__appear={animate === 'appear'}
+	class:is-from-center={is_from_center}
+	class:is-from-top={is_from_top}
+	class:is-without-arrow={is_without_arrow}
+>
+	<div class="{class_prefix}__content">
+		<slot />
 	</div>
 </div>
