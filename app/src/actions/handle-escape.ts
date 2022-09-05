131f186e8bc6f3d749937f51b14f99a80a707aea
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-05 20:20:43 +0700

    Add action: handle_escape

---
+++ app/src/actions/handle-escape.ts
@@ -0,0 +1,38 @@
+import type { ActionReturn } from 'svelte/action';
+
+interface Params {
+	callback: () => void;
+}
+
+export function handle_escape( node: HTMLElement, params: Params ): ActionReturn< Params > {
+	let current_callback: Params[ 'callback' ];
+
+	const handle_keydown = ( event: KeyboardEvent ): void => {
+		if ( event.code === 'Escape' ) {
+			current_callback();
+		}
+
+		console.log( 'boo' );
+	};
+
+	const toggle = ( active: boolean, current_params: Params ): void => {
+		if ( active ) {
+			current_callback = current_params.callback;
+			node.addEventListener( 'keydown', handle_keydown, true );
+		} else {
+			node.removeEventListener( 'keydown', handle_keydown, true );
+		}
+	};
+
+	const destroy = (): void => toggle( false, { callback: current_callback } );
+
+	toggle( true, params );
+
+	return {
+		destroy,
+		update( next_params: Params ): void {
+			destroy();
+			toggle( true, next_params );
+		},
+	};
+}
