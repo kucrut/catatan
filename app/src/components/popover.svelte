6ec284dd48d3d4063ecfd97f671a3fd91ba9dac0
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-05 19:42:44 +0700

    Popover: Use click_outside action, dispatch close event

--- app/src/components/popover.svelte
+++ app/src/components/popover.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
+	import { click_outside } from '$actions/click-outside';
+	import { createEventDispatcher } from 'svelte';
+
 	export let animate: null | 'appear' = null;
 	export let is_from_center = false;
 	export let is_from_top = false;
 	export let is_without_arrow = false;
 
+	const dispatch = createEventDispatcher< { close: undefined } >();
 	const class_prefix = 'components-popover';
 </script>
 
@@ -14,6 +18,7 @@
 	class:is-from-center={is_from_center}
 	class:is-from-top={is_from_top}
 	class:is-without-arrow={is_without_arrow}
+	use:click_outside={{ active: true, callback: () => dispatch( 'close' ) }}
 >
 	<div class="{class_prefix}__content">
 		<slot />
