b4f4ac48230e4bac78128285b344eab059e55adc
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-08 04:48:48 +0700

    Improve trap-focus action

--- app/src/actions/trap-focus.ts
+++ app/src/actions/trap-focus.ts
@@ -1,34 +1,41 @@
 import type { ActionReturn } from 'svelte/action';
 
-function get_focusable_elements( element: HTMLElement ): Element[] {
-	return [
-		...element.querySelectorAll( '[tabindex]:not([tabindex="-1"]), a[href], button, details, input, select, textarea' ),
-	].filter(
-		el => ! el.hasAttribute( 'disabled' ) && ! el.hasAttribute( 'hidden' ) && ! el.getAttribute( 'aria-hidden' ),
-	);
-}
-
 export function trap_focus( node: HTMLElement ): ActionReturn< undefined > {
 	function handle_keydown( event: KeyboardEvent ): void {
 		if ( event.code !== 'Tab' ) {
 			return;
 		}
 
-		const focusable_els = get_focusable_elements( node );
-		const last_focusable_el = focusable_els[ focusable_els.length - 1 ];
-		const active_el = node.ownerDocument.activeElement;
-		let el_to_focus: HTMLAnchorElement;
+		event.preventDefault();
 
-		if ( event.shiftKey && active_el === focusable_els[ 0 ] ) {
-			el_to_focus = last_focusable_el as HTMLAnchorElement;
-		} else if ( active_el === last_focusable_el ) {
-			el_to_focus = focusable_els[ 0 ] as HTMLAnchorElement;
+		const tabbables = Array.from( node.querySelectorAll( '*' ) ).filter( el => {
+			return (
+				'tabIndex' in el &&
+				// @ts-expect-error The element must have tabIndex attribute after the check above.
+				el.tabIndex >= 0 &&
+				! el.hasAttribute( 'disabled' ) &&
+				! el.hasAttribute( 'hidden' ) &&
+				! el.getAttribute( 'aria-hidden' )
+			);
+		} );
+
+		if ( ! tabbables.length ) {
+			return;
 		}
 
-		if ( el_to_focus ) {
-			event.preventDefault();
-			el_to_focus.focus();
+		// Index of element that's currently in focus.
+		let index = tabbables.indexOf( node.ownerDocument.activeElement );
+
+		// The focus is outside. Reset it.
+		if ( index === -1 ) {
+			index = 0;
 		}
+
+		index += tabbables.length + ( event.shiftKey ? -1 : 1 );
+		index %= tabbables.length;
+
+		// @ts-expect-error This is fine.
+		tabbables[ index ].focus();
 	}
 
 	function toggle_listener( should_listen: boolean ): void {
