e9eeb4f47104c2fe517bd583e24d68d3f7048b7c
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-09 08:32:59 +0700

    Blocks store: Add image_default_size, image_size_names props

--- app/src/stores/blocks.ts
+++ app/src/stores/blocks.ts
@@ -1,13 +1,19 @@
+import type { Config } from '$types';
 import type { Editor } from '@tiptap/core';
 import { writable, type Writable } from 'svelte/store';
 
-export interface Blocks {
+export type BlocksStoreParams = Pick< Config, 'image_default_size' | 'image_size_names' >;
+
+export interface Blocks extends BlocksStoreParams {
 	edited_link: null | string;
 	editor?: Editor;
 }
 
 export type BlocksStore = Writable< Blocks >;
 
-export default function create_store(): BlocksStore {
-	return writable< Blocks >( { edited_link: null } );
+export default function create_store( params: BlocksStoreParams ): BlocksStore {
+	return writable< Blocks >( {
+		...params,
+		edited_link: null,
+	} );
 }
