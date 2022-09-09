e2c1df2f99bce24024fd6d96807ef43e99122449
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-09 07:34:47 +0700

    Blocks store: Add prop: edited_link

--- app/src/stores/blocks.ts
+++ app/src/stores/blocks.ts
@@ -2,11 +2,12 @@ import type { Editor } from '@tiptap/core';
 import { writable, type Writable } from 'svelte/store';
 
 export interface Blocks {
+	edited_link: null | string;
 	editor?: Editor;
 }
 
 export type BlocksStore = Writable< Blocks >;
 
 export default function create_store(): BlocksStore {
-	return writable< Blocks >( {} );
+	return writable< Blocks >( { edited_link: null } );
 }
