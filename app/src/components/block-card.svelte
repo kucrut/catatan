70e6e7b170103fd224467b2a00043e997df24313
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-09 07:20:29 +0700

    Add component: BlockCard

---
+++ app/src/components/block-card.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import Icon from './icons/icon.svelte';
+
+	export let description: string;
+	export let icon = '';
+	export let title: string;
+
+	const class_prefix = 'block-editor-block-card';
+</script>
+
+<div class={class_prefix}>
+	{#if icon}
+		<span class="block-editor-block-icon"><Icon {icon} /></span>
+	{/if}
+	<div class="{class_prefix}__content">
+		<h2 class="{class_prefix}__title">{title}</h2>
+		<span class="{class_prefix}__description">{description}</span>
+	</div>
+</div>
