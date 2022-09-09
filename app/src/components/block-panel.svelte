252fb1e13dc4f1309bf8c0425328b0f50f44bc73
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-09 07:29:31 +0700

    BlockPanel: Render WpImageBlockInspector

--- app/src/components/block-panel.svelte
+++ app/src/components/block-panel.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
 	import { __ } from '@wordpress/i18n';
+	import { get_store } from '$stores';
+	import WpImageBlockInspector from './wp-image-block-inspector.svelte';
 
 	export let hidden = false;
+
+	const blocks = get_store( 'blocks' );
 </script>
 
-<div {hidden} class="components-panel">
-	<span class="block-editor-block-inspector__no-blocks">{__( 'No block selected.' )}</span>
-</div>
+{#if $blocks.editor}
+	<div {hidden} class="components-panel">
+		{#if $blocks.editor.isActive( 'wpImage' )}
+			<WpImageBlockInspector />
+		{:else}
+			<span class="block-editor-block-inspector__no-blocks">{__( 'No block selected.' )}</span>
+		{/if}
+	</div>
+{/if}
