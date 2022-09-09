2aa42a87be40deee3678088512068d4eca25327a
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-09 09:07:39 +0700

    Add component: BaseControlHelp

---
+++ app/src/components/base-control-help.svelte
@@ -0,0 +1,12 @@
+<p class="components-base-control__help">
+	<slot />
+</p>
+
+<style>
+	.components-base-control__help {
+		font-size: 12px;
+		font-style: normal;
+		color: rgb( 117, 117, 117 );
+		margin-block-end: revert;
+	}
+</style>
