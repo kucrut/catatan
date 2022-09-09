3a30e142ab586332ba4ff6488fb2f21c7061173f
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-09 07:29:11 +0700

    Add component: WpImageBlockInspector

---
+++ app/src/components/wp-image-block-inspector.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { __ } from '@wordpress/i18n';
+	import BlockCard from './block-card.svelte';
+</script>
+
+<div class="block-editor-block-inspector">
+	<BlockCard description={__( 'Insert an image to make a visual statement.' )} icon="image" title={__( 'Image' )} />
+</div>
